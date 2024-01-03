dfe9f597385ea99832463462150d78cb4b8c7601
Sadiq <seeker99157@gmail.com>
2024-01-03 11:50:35 +0100

    added featured article

--- app/_components/Hero/HeroSection.tsx
+++ app/_components/Hero/HeroSection.tsx
@@ -7,13 +7,13 @@ const HeroSection = (props: Props) => {
         <div className="flex justify-center">
             <div className="max-w-3xl px-5 md:px-0 text-center py-20">
                 <p className="uppercase pb-4 font-medium">Featured article</p>
-                <h2 className=" text-3xl md:text-5xl lg:text-7xl font-semibold">Lorem ipsum dolor sit amet.dolor sit amet.</h2>
+                <h2 className=" text-3xl md:text-5xl lg:text-7xl font-semibold">202-400 goals.</h2>
                 <div className="flex justify-center gap-2 py-8">
-                    <Link href={'/'} className="font-medium hover:text-orange-500">By Sadiq Bilyamin /</Link>
-                    <Link href={'/'} className="font-medium hover:text-orange-500">In Culture /</Link>
+                    <Link href={'/account/4Z1sACnghdPQM3ugrCdDHOo9i0s1'} className="font-medium hover:text-orange-500">By Nafisa Isah /</Link>
+                    <Link href={'/articles'} className="font-medium hover:text-orange-500">In Articles /</Link>
                 </div>
                 <div className="uppercase flex gap-4 justify-center">
-                    <Link href={'articles/OvWsgKb1mOYXlcqpwvXF'} className="duration-200 ease-in  w-32 text-sm font-medium hover:bg-white hover:text-black hover:border-2 hover:border-orange-500 shadow-black px-4 py-2 bg-orange-500 text-white">Read</Link>
+                    <Link href={'articles/oOjRRM92i4JNIHlR8lbi'} className="duration-200 ease-in  w-32 text-sm font-medium hover:bg-white hover:text-black hover:border-2 hover:border-orange-500 shadow-black px-4 py-2 bg-orange-500 text-white">Read</Link>
                     {/* <button className="duration-200 ease-in flex gap-1 items-center border-2 border-orange-500 px-4 py-2 w-32 text-sm font-medium hover:shadow-2xl shadow-black hover:bg-orange-500 hover:text-white"><BsBookmarks size={15} />Read Later</button> */}
                 </div>
             </div>
